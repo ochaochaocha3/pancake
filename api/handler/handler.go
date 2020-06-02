6ec378400ef633cff5f5c0848064fb1a378a1b10
ocha <ochaochaocha3@gmail.com>
2020-06-03 03:49:03 +0900

    サーバを実装する

---
+++ api/handler/handler.go
@@ -0,0 +1,92 @@
+package handler
+
+import (
+	"context"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/ochaochaocha3/pancake/api/gen/api"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+type BakerHandler struct {
+	report *report
+}
+
+type report struct {
+	sync.Mutex
+	data map[api.Pancake_Menu]int
+}
+
+func NewBakerHandler() *BakerHandler {
+	return &BakerHandler{
+		report: &report{
+			data: map[api.Pancake_Menu]int{},
+		},
+	}
+}
+
+func (h *BakerHandler) Bake(
+	ctx context.Context,
+	req *api.BakeRequest,
+) (*api.BakeResponse, error) {
+	if req.Menu == api.Pancake_UNKNOWN || req.Menu > api.Pancake_SPICY_CURRY {
+		return nil, status.Errorf(codes.InvalidArgument, "パンケーキを選んでください!")
+	}
+
+	now := time.Now()
+
+	{
+		h.report.Lock()
+
+		h.report.data[req.Menu] += 1
+
+		h.report.Unlock()
+	}
+
+	return &api.BakeResponse{
+		Pancake: &api.Pancake{
+			Menu:           req.Menu,
+			ChefName:       "ocha",
+			TechnicalScore: rand.Float32(),
+			CreateTime: &timestamp.Timestamp{
+				Seconds: now.Unix(),
+				Nanos:   int32(now.Nanosecond()),
+			},
+		},
+	}, nil
+}
+
+func (h *BakerHandler) Report(
+	ctx context.Context,
+	req *api.ReportRequest,
+) (*api.ReportResponse, error) {
+	var counts []*api.Report_BakeCount
+
+	{
+		h.report.Lock()
+
+		for k, v := range h.report.data {
+			counts = append(counts, &api.Report_BakeCount{
+				Menu:  k,
+				Count: int32(v),
+			})
+		}
+
+		h.report.Unlock()
+	}
+
+	return &api.ReportResponse{
+		Report: &api.Report{
+			BakeCounts: counts,
+		},
+	}, nil
+}
