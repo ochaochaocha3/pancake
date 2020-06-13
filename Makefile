e569c713aba77bbc24016634cc52d86383213837
ocha <ochaochaocha3@gmail.com>
2020-06-14 04:15:25 +0900

    Rubyでサーバを作る

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@
 
 all: proto_boilerplate
 
-proto_boilerplate: api/gen/api/pancake.pb.go client/lib/pancake/pancake_pb.rb
+proto_boilerplate: \
+  api/gen/api/pancake.pb.go \
+  api_ruby/lib/pancake/pancake_pb.rb \
+  client/lib/pancake/pancake_pb.rb
 
 api/gen/api/pancake.pb.go: proto/pancake.proto
 	protoc -Iproto --go_out=plugins=grpc:api proto/*.proto
+
+api_ruby/lib/pancake/pancake_pb.rb: proto/pancake.proto
+	cd api_ruby && make proto_boilerplate
 
 client/lib/pancake/pancake_pb.rb: proto/pancake.proto
 	cd client && make proto_boilerplate
---
+++ api_ruby/lib/pancake/pancake_baker.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require_relative 'pancake_pb'
+require_relative 'pancake_services_pb'
+
+module Pancake
+  class Baker < Pancake::PancakeBakerService::Service
+    class Report
+      def initialize
+        @mutex = Mutex.new
+        @counts = {}
+      end
+
+      def inc(menu)
+        @mutex.synchronize do
+          @counts[menu] ||= 0
+          @counts[menu] += 1
+        end
+      end
+
+      def to_proto_obj
+        bake_counts = @mutex.synchronize do
+          @counts.map { |k, v|
+            ::Pancake::Report::BakeCount.new(menu: k, count: v)
+          }
+        end
+
+        ::Pancake::Report.new(bake_counts: bake_counts)
+      end
+    end
+
+    def initialize(*)
+      super
+
+      @report = Report.new
+    end
+
+    def bake(request, _call)
+      if request.menu == :UNKNOWN
+        raise GRPC::StatusError.new(
+          GRPC::Status::INVALID_ARGUMENT,
+          'パンケーキを選んでください'
+        )
+      end
+
+      now = Time.now
+      seconds = now.to_i
+      nanos = ((now.to_f - seconds) * 1e9).to_i
+
+      @report.inc(request.menu)
+
+      BakeResponse.new(
+        pancake: Pancake.new(
+          menu: request.menu,
+          chef_name: 'ocha',
+          technical_score: rand(),
+          create_time: Google::Protobuf::Timestamp.new(
+            seconds: seconds,
+            nanos: nanos
+          )
+        )
+      )
+    end
+
+    def report(_request, _call)
+      ReportResponse.new(report: @report.to_proto_obj)
+    end
+  end
+end
